0ff0328c34d121d2ae9dd9f1476836532394cf10
Matt Enlow <matt@divaos.com>
2015-03-01 20:47:26 -0700

    Related handlebarsized

--- engage/app/fixtures/related.js
+++ engage/app/fixtures/related.js
@@ -1,25 +1,33 @@
 export default [{
+  id: 1,
   title: 'Jackson Hole Community Pathways',
   url: 'tetonwyo.org/pathway'
 }, {
+  id: 2,
   title: 'Friends of Pathways',
   url: 'friendsofpathways.org'
 }, {
+  id: 3,
   title: 'Path22 on Facebook',
   url: 'facebook.com/path22'
 }, {
+  id: 4,
   title: 'Teton County Parks and Recreation',
   url: 'www.tetonparksandrec.org'
 }, {
+  id: 5,
   title: 'Website Link Goes Here',
   url: 'www.example.com'
 }, {
+  id: 6,
   title: 'Website Link Goes Here',
   url: 'www.example.com'
 }, {
+  id: 7,
   title: 'Website Link Goes Here',
   url: 'www.example.com'
 }, {
+  id: 8,
   title: 'List group item heading',
   url: 'www.example.com'
 }];
--- engage/app/models/link.js
+++ engage/app/models/link.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  url: DS.attr('string'),
-  date: DS.attr('date')
+  date: DS.attr('date'),
+  title: DS.attr('string'),
+  url: DS.attr('string')
 });
