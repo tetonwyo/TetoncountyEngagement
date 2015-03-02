94eda7e626867326e65172d163d1b1be2e4e413a
Matt Enlow <matt@divaos.com>
2015-03-01 18:41:06 -0700

    Remove old fixtures

--- engage/app/fixtures/tags.js
+++
@@ -1,6 +0,0 @@
-export default [
-  {"id":"sitefeatured","title":"Featured Item","type":"General"},
-  {"id":"adminsitefeatured","title":"Admin Featured Item","type":"General"},
-  {"id":"video","title":"Video","type":"General"},
-  {"id":"hubpage","title":"Hub Page","type":"General"}
-];
