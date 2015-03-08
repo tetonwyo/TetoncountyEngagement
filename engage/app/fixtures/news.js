ce3e21514d0e0a7ffc52111d3b1f0df0bdbe8a0d
Matt Enlow <matt@divaos.com>
2015-03-08 15:18:54 -0600

    News fixtures

--- engage/app/fixtures/news.js
+++ engage/app/fixtures/news.js
@@ -1,3 +1,32 @@
 export default [
-
+  {
+    id: 1,
+    body: 'Esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'News Headline',
+    date: 'Mar 13, 2015'
+  },
+  {
+    id: 2,
+    body: 'Ut enim ad minim veniam, quis nostrud exercitation cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'News Headline',
+    date: 'Feb 13, 2015'
+  },
+  {
+    id: 3,
+    body: 'Lorem ipsum dolor sit amet, ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'News Headline',
+    date: 'Jan 13, 2015'
+  },
+  {
+    id: 4,
+    body: 'Ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'News Headline',
+    date: 'Dec 13, 2014'
+  },
+  {
+    id: 5,
+    body: 'Sed do eiusmod tempor incididunt ut veniam, quis nostrud exercitation ullamco laboris nisi cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'News Headline',
+    date: 'Mar 13, 2015'
+  }
 ];
