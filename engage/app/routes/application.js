7e3e67ccae1e8ae811b795f00d786708f68dea18
Matt Enlow <matt@divaos.com>
2015-03-11 19:38:21 -0600

    Move sidebar to application.hbs

--- engage/app/routes/application.js
+++ engage/app/routes/application.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+  actions: {
+    showSidebar() {
+      this.controller.set('showSidebar', true);
+    },
+    hideSidebar() {
+      this.controller.set('showSidebar', false);
+    }
+  }
 });
