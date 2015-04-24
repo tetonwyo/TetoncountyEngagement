82d833644d650e55c5a5d60615565ff28e1ae771
Matt Enlow <matt@divaos.com>
2015-04-24 14:30:26 +0200

    Use live news endpoint

--- engage/app/routes/news.js
+++ engage/app/routes/news.js
@@ -1,7 +1,17 @@
+import Ember from 'ember';
 import Route from 'engage/routes/section-briefs-route';
 
 export default Route.extend({
   model () {
-    return this.store.find('news');
+    return Ember.$.getJSON('http://www.tetonwyo.org/data/news.ashx')
+      .then(response => response.map(news => {
+        return {
+          id: news.NewsId,
+          title: news.Title,
+          date: news.DateEntered,
+          media: news.MainImageURL,
+          body: news.BriefDescr
+        };
+      }));
   }
 });
