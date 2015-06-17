41b69e4fc2eec6b97b8c22d03b62263c2535bbf6
bgphipps <bgphipps@gmail.com>
2015-06-16 21:36:15 -0600

    wrapped algoliasearch in document ready. modified templates

--- js/algoliasearch.js
+++ js/algoliasearch.js
@@ -6,61 +6,62 @@
  */
 
 
+jQuery(document).ready(function(){
+    var client = algoliasearch('P71J7RF23K', '20ceae787550b54ab727a6129342f4c1');
 
-var client = algoliasearch('P71J7RF23K', '20ceae787550b54ab727a6129342f4c1');
+    //
+    // Bind the searchBox search event to keyup.
+    //
+    jQuery("#searchInput").on('keyup', function(){
+        var queryString = jQuery("#searchInput").val();
 
-//
-// Bind the searchBox search event to keyup.
-//
-jQuery("#searchInput").on('keyup', function(){
-    var queryString = jQuery("#searchInput").val();
+        if(queryString === ""){
+            jQuery("#searchResultsContainer").html("");
+        }
+        else{
+            a_search(queryString);
+        }
+    });
 
-    if(queryString === ""){
-        jQuery("#searchResultsContainer").html("");
+
+
+    //
+    // Function a_search
+    // @query : string query for searching
+    //
+    function a_search(query){
+
+        var queries = [{
+            indexName: 'dev_tetonwyo',
+            query: query,
+            params: {hitsPerPage: 3}
+        }, {
+            indexName: 'dev_tetonwyo_documents',
+            query: query,
+            params: {hitsPerPage: 5, distinct: true}
+        }];
+
+        client.search(queries, searchMultiCallback);
+
     }
-    else{
-        a_search(queryString);
+
+
+    function searchMultiCallback(err, content) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        var results = {};
+        results.web = content.results[0].hits;
+        results.docs = content.results[1].hits;
+
+        console.log(results);
+
+        var source   = $("#results-template").html();
+        var template = Handlebars.compile(source);
+
+        var context = results;
+        var html    = template(context);
+        jQuery("#searchResultsContainer").html(html);
     }
 });
-
-
-
-//
-// Function a_search
-// @query : string query for searching
-//
-function a_search(query){
-
-    var queries = [{
-        indexName: 'dev_tetonwyo',
-        query: query,
-        params: {hitsPerPage: 3}
-    }, {
-        indexName: 'dev_tetonwyo_documents',
-        query: query,
-        params: {hitsPerPage: 5, distinct: true}
-    }];
-
-    client.search(queries, searchMultiCallback);
-
-}
-
-
-function searchMultiCallback(err, content) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    var results = {};
-    results.web = content.results[0].hits;
-    results.docs = content.results[1].hits;
-
-    console.log(results);
-
-    var source   = $("#results-template").html();
-    var template = Handlebars.compile(source);
-
-    var context = results;
-    var html    = template(context);
-    jQuery("#searchResultsContainer").html(html);
-}
