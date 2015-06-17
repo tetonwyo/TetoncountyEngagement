fe27aa9870b1d238e3378a3cc30141ea9d8af64e
bgphipps <bgphipps@gmail.com>
2015-06-16 23:02:17 -0600

    modified templates. Added logic for templating multiple sections. Added broken logic for more section.

--- js/algoliasearch.js
+++ js/algoliasearch.js
@@ -6,8 +6,24 @@
  */
 
 
+Handlebars.registerHelper('each_upto', function(ary, start, max, options) {
+    if(!ary || ary.length == 0)
+        return options.inverse(this);
+
+    if(!start || typeof start == "undefined")
+        start = 0;
+
+    var result = [ ];
+    for(start; start < max && start < ary.length; ++start)
+        result.push(options.fn(ary[start]));
+    return result.join('');
+});
+
+
 jQuery(document).ready(function(){
+    var moreResults = "web";
     var client = algoliasearch('P71J7RF23K', '20ceae787550b54ab727a6129342f4c1');
+    var $templateContainers = jQuery("#docResultsContainer, #webResultsContainer");
 
     //
     // Bind the searchBox search event to keyup.
@@ -16,7 +32,7 @@ jQuery(document).ready(function(){
         var queryString = jQuery("#searchInput").val();
 
         if(queryString === ""){
-            jQuery("#searchResultsContainer").html("");
+            $templateContainers.html("");
         }
         else{
             a_search(queryString);
@@ -34,11 +50,11 @@ jQuery(document).ready(function(){
         var queries = [{
             indexName: 'dev_tetonwyo',
             query: query,
-            params: {hitsPerPage: 3, attributesToRetrieve: "Url,Description,SubTitle,Description,PublishDate,DepartmentName,Type"}
+            params: {hitsPerPage: 10, attributesToRetrieve: "Url,Description,SubTitle,Description,PublishDate,DepartmentName,Type"}
         }, {
             indexName: 'dev_tetonwyo_documents',
             query: query,
-            params: {hitsPerPage: 5, distinct: true, attributesToRetrieve: "parentTitle,url,title,meetingTitle"}
+            params: {hitsPerPage: 10, distinct: true, attributesToRetrieve: "parentTitle,url,title,meetingTitle"}
         }];
 
         client.search(queries, searchMultiCallback);
@@ -51,18 +67,45 @@ jQuery(document).ready(function(){
             console.error(err);
             return;
         }
-        
+
         var results = {};
         results.web = content.results[0].hits;
         results.docs = content.results[1].hits;
 
         console.log(results);
 
-        var source   = $("#results-template").html();
-        var template = Handlebars.compile(source);
+        var webResults   = $("#webResultsTemplate").html();
+        var docResults   = $("#docResultsTemplate").html();
 
-        var context = results;
-        var html    = template(context);
-        jQuery("#searchResultsContainer").html(html);
+        var webResultsTemplate = Handlebars.compile(webResults);
+        var docResultsTemplate = Handlebars.compile(docResults);
+
+        var webResultsHtml    = webResultsTemplate(results);
+        var docResultsHtml    = docResultsTemplate(results);
+        
+        // Apply our web results template
+        jQuery("#webResultsContainer").html(webResultsHtml);
+
+        // Apply our doc results template
+        jQuery("#docResultsContainer").html(docResultsHtml);
+
+        // Apply the rest of either docs or web based on variable.
+        if(moreResults == "docs")
+        {
+            console.log("test");
+            var docResultsMore = $("#docResultsTemplateMore").html();
+            var docResultsTemplateMore = Handlebars.compile(docResultsMore);
+            var docResultsMoreHtml    = docResultsTemplateMore(results);
+            jQuery("#moreResultsContainer").html(docResultsMoreHtml);
+        }
+        else if(moreResults == "web")
+        {
+            console.log("test");
+            var webResultsMore = $("#webResultsTemplateMore").html();
+            var webResultsTemplateMore = Handlebars.compile(webResultsMore);
+            var webResultsMoreHtml    = webResultsTemplateMore(results);
+            jQuery("#moreResultsContainer").html(webResultsMoreHtml);
+        }
+
     }
 });
