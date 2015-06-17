aadd660e30680eca96a9d95e78ea6b2c1f2b2731
Stine Richvoldsen <superrunt@gmail.com>
2015-06-16 23:14:36 -0600

    added som ecode for sorting

--- js/algoliasearch.js
+++ js/algoliasearch.js
@@ -5,21 +5,19 @@
  * Time: 9:07 PM
  */
 
-
 var client = algoliasearch('P71J7RF23K', '20ceae787550b54ab727a6129342f4c1');
 
 var initAndDisplayAlgoliaSearch = function ( targetElement ) {
 
     var _target = $(targetElement)
     // add all the DOM elements we need
+    // this is where the sorting button should be added
     _target.append('<div id="searchForm"><input id="searchInput" type="text" name="fname" placeholder="Enter Search Here"></div><div id="searchResultsContainer"></div>')
 
     // get handlebar template and 'fill out'
     $.get("/templates/algoliasearch_results.hbs", function ( source ) {
 
-        //
         // Bind the searchBox search event to keyup.
-        //
         jQuery("#searchInput").on('keyup', function(){
             var queryString = jQuery("#searchInput").val();
 
@@ -31,11 +29,31 @@ var initAndDisplayAlgoliaSearch = function ( targetElement ) {
             }
         });
 
+        // Pseudo code for binding a sirt function to a sort button
+        jQuery("#mySortingButton").on('click', function() {
+
+            var _this = $(this),
+                _indexAddon = _this.attr("data-addon");
+
+            var queryString = jQuery("#searchInput").val();
+            if(queryString === ""){
+                jQuery("#searchResultsContainer").html("");
+            }
+            else{
+                a_search(queryString, _indexAddon);
+            }
+        });
+
         //
         // Function a_search
         // @query : string query for searching
         //
-        function a_search(query){
+        function a_search(query, indexAddon){
+
+            // TODO: add indexAddon here
+            if ( indexAddon ) {
+                // create string for indexName attributes below
+            }
 
             var queries = [{
                 indexName: 'dev_tetonwyo',
