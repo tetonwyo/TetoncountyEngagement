6bc4ba03644ee12061d3738a9af885a3faa020bf
Stine Richvoldsen <superrunt@gmail.com>
2015-06-16 21:09:42 -0600

    moved js into separtate file

---
+++ js/algoliasearch.js
@@ -0,0 +1,71 @@
+/**
+ * Created with RubyMine.
+ * By: superrunt
+ * Date: 6/16/15
+ * Time: 9:07 PM
+ */
+
+
+
+var client = algoliasearch('P71J7RF23K', '20ceae787550b54ab727a6129342f4c1');
+
+//
+// Bind the searchBox search event to keyup.
+//
+jQuery("#searchInput").on('keyup', function(){
+    var queryString = jQuery("#searchInput").val();
+
+    if(queryString === ""){
+        jQuery("#searchResultsContainer").html("");
+    }
+    else{
+        a_search(queryString);
+    }
+});
+
+
+
+//
+// Function a_search
+// @query : string query for searching
+//
+function a_search(query){
+
+    var queries = [{
+        indexName: 'dev_tetonwyo',
+        query: query,
+        params: {hitsPerPage: 3}
+    }, {
+        indexName: 'dev_tetonwyo_documents',
+        query: query,
+        params: {hitsPerPage: 5, distinct: true}
+    }];
+
+    client.search(queries, searchMultiCallback);
+
+}
+
+
+function searchMultiCallback(err, content) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    var categories = content.results[0];
+    for (var i = 0; i < categories.hits.length; ++i) {
+        console.log(categories.hits[i]);
+    }
+
+    var products = content.results[1];
+    for (var i = 0; i < products.hits.length; ++i) {
+        console.log(products.hits[i]);
+    }
+
+    var source   = $("#results-template").html();
+    var template = Handlebars.compile(source);
+
+    var context = categories;
+    var html    = template(context);
+    jQuery("#searchResultsContainer").html(html);
+}
