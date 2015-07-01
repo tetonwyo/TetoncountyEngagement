ba7015789617d9349dbd98ea72c117c454973d93
Stine Richvoldsen <superrunt@gmail.com>
2015-06-30 22:19:39 -0600

    fixed more template bug, and wired up more buttons

--- js/algoliasearch.js
+++ js/algoliasearch.js
@@ -7,6 +7,7 @@
 
 
 Handlebars.registerHelper('each_upto', function(ary, start, max, options) {
+
     if(!ary || ary.length == 0)
         return options.inverse(this);
 
@@ -14,14 +15,17 @@ Handlebars.registerHelper('each_upto', function(ary, start, max, options) {
         start = 0;
 
     var result = [ ];
+
     for(start; start < max && start < ary.length; ++start)
         result.push(options.fn(ary[start]));
+
     return result.join('');
 });
 
 
 
 var moreResults = "web";
+var lastResults;
 var client = algoliasearch('P71J7RF23K', '20ceae787550b54ab727a6129342f4c1');
 var $templateContainers = jQuery("#docResultsContainer, #webResultsContainer");
 
@@ -30,16 +34,17 @@ var $templateContainers = jQuery("#docResultsContainer, #webResultsContainer");
 // Function a_search
 // @query : string query for searching
 //
-function a_search(query, indexAddon){
+function a_search(query, indexAddon, limit){
     if ( !indexAddon ) indexAddon = "";
+    if ( !limit ) limit = 10;
     var queries = [{
         indexName: 'dev_tetonwyo' + indexAddon,
         query: query,
-        params: {hitsPerPage: 3, attributesToRetrieve: "Url,Description,SubTitle,Description,PublishDate,DepartmentName,Type"}
+        params: {hitsPerPage: limit, attributesToRetrieve: "Url,Description,SubTitle,Description,PublishDate,DepartmentName,Type"}
     }, {
         indexName: 'dev_tetonwyo_documents',
         query: query,
-        params: {hitsPerPage: 5, distinct: true, attributesToRetrieve: "parentTitle,url,title,meetingTitle"}
+        params: {hitsPerPage: limit, distinct: true, attributesToRetrieve: "parentTitle,url,title,meetingTitle"}
     }];
 
     client.search(queries, searchMultiCallback);
@@ -59,8 +64,10 @@ function searchMultiCallback(err, content) {
 
         console.log(results);
 
-        var webResults   = $("#webResultsTemplate").html();
-        var docResults   = $("#docResultsTemplate").html();
+        var webResults      = $("#webResultsTemplate").html();
+        var webResultsMore  = $("#webResultsTemplateMore").html();
+        var docResults      = $("#docResultsTemplate").html();
+        var docResultsMore  = $("#docResultsTemplateMore").html();
 
         var webResultsTemplate = Handlebars.compile(webResults);
         var docResultsTemplate = Handlebars.compile(docResults);
@@ -74,26 +81,42 @@ function searchMultiCallback(err, content) {
         // Apply our doc results template
         jQuery("#docResultsContainer").html(docResultsHtml);
 
-        // TODO: Apply the rest of either docs or web based on variable.
-        if(moreResults == "docs")
-        {
-            console.log("doc");
-            var docResultsMore = $("#docResultsTemplateMore").html();
-            var docResultsTemplateMore = Handlebars.compile(docResultsMore);
-            var docResultsMoreHtml    = docResultsTemplateMore(results);
-            jQuery("#moreResultsContainer").html(docResultsMoreHtml);
-        }
-        else if(moreResults == "web")
-        {
-            console.log("web");
-            var webResultsMore = $("#webResultsTemplateMore").html();
-            var webResultsTemplateMore = Handlebars.compile(webResultsMore);
-            var webResultsMoreHtml    = webResultsTemplateMore(results);
-            jQuery("#moreResultsContainer").html(webResultsMoreHtml);
-        }
+        displayMoreBlock (moreResults, results);
     }
 
 }
+
+function displayMoreBlock ( moreOf, results ) {
+    // TODO: fix this!
+    if ( results ) {
+        lastResults = results
+    } else if (lastResults) {
+        results = lastResults
+    } else {
+        console.log("nothing to display")
+        return
+    }
+
+    var _currentlyVisible = $("div.moreResultsContainer div.results");
+
+    if (moreOf == "docs" && !_currentlyVisible.hasClass("doc"))
+    {
+        console.log("more doc");
+        var docResultsMore = $("#docResultsTemplateMore").html();
+        var docResultsTemplateMore = Handlebars.compile(docResultsMore);
+        var docResultsMoreHtml    = docResultsTemplateMore(results);
+        jQuery("#moreResultsContainer").html(docResultsMoreHtml);
+    }
+    else if (moreOf == "web" && !_currentlyVisible.hasClass("web"))
+    {
+        console.log("more web");
+        var webResultsMore = $("#webResultsTemplateMore").html();
+        var webResultsTemplateMore = Handlebars.compile(webResultsMore);
+        var webResultsMoreHtml    = webResultsTemplateMore(results);
+        jQuery("#moreResultsContainer").html(webResultsMoreHtml);
+    }
+}
+
 
 
 var initAndDisplayAlgoliaSearch = function ( targetElement ) {
@@ -102,7 +125,7 @@ var initAndDisplayAlgoliaSearch = function ( targetElement ) {
     // add all the DOM elements we need
     // this is where the sorting button should be added
     _target.append('<div id="searchForm"><input id="searchInput" type="text" name="fname" placeholder="Enter Search Here"></div><div class="row">'
-                +'<div id="webResultsContainer"></div></div><div class="row"><div id="docResultsContainer"></div></div><div class="row"><div id="moreResultsContainer"></div>'
+                +'<div id="webResultsContainer"></div><button class="more" data-moreof="web">More Web Results</button></div><div class="row"><div id="docResultsContainer"></div><button class="more" data-moreof="docs">More Doc Results</button></div><div class="row"><div id="moreResultsContainer"></div>'
                                 +'</div></div>');
 
 
@@ -122,7 +145,7 @@ var initAndDisplayAlgoliaSearch = function ( targetElement ) {
             }
         });
 
-        // Pseudo code for binding a sort function to a sort button
+        // sort button
         jQuery("button.sort").on('click', function() {
             // if no search query present, just return
             if ( $("#searchInput").val() === "" ) return
@@ -137,6 +160,14 @@ var initAndDisplayAlgoliaSearch = function ( targetElement ) {
             else{
                 a_search(queryString, _indexAddon);
             }
+        });
+
+        // more buttons
+        jQuery("button.more").on('click', function() {
+            var _this = $(this),
+                _moreOf = _this.attr("data-moreof");
+
+            displayMoreBlock(_moreOf)
         });
 
         //
